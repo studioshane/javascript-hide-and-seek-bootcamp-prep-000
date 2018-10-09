db30c51e7c7d827a8be94aa4fac3d75d796dac54
Shane Walsh <studioshane@gmail.com>
2018-10-09 13:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target")
+}
+
+
+function increaseRankBy(n) {
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+
+function deepestChild(){
+  
+}
