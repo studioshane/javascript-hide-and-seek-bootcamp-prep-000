3aece091d536d2815b326728ca5b8cf8104755cf
Shane Walsh <studioshane@gmail.com>
2018-10-09 13:38:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target")
+}
+
+
+function increaseRankBy(n) {
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var array=[];
+  for(let i=0;i<lis.length-1;i++) {
+    array.push(lis[i].querySelector("div"));
+  }
+return array[array.length-1].innerHTML;
+}
 
